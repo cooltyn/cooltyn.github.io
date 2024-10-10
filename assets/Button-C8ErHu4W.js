761e6117861a6343931f3b46a369c8b211b9ff1c
Sergey Pudyakov <cooltyn@mail.ru>
2024-10-10 23:56:45 +0500

    0.1.3

---
+++ assets/Button-C8ErHu4W.js
@@ -0,0 +1,1 @@
+import{_ as s,o as t,c as e,r,n as c}from"./index-CWxyfhef.js";const n="_root_1u73o_1",a={root:n},l={};function _(o,d){return t(),e("div",{class:c(o.$style.root),ontouchstart:""},[r(o.$slots,"default")],2)}const u={$style:a},i=s(l,[["render",_],["__cssModules",u]]);export{i as B};
